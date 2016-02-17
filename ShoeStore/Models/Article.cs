450d53de6bca408ab1a294c3c0edb8ce5ad55c14
maxzo <sariego.isi@gmail.com>
2016-02-17 16:50:02 -0300

    First commit with working UI.

---
+++ ShoeStore/Models/Article.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Linq;
+using System.Web;
+
+namespace ShoeStore.Models
+{
+    public class Article
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+        [Display(Name = "Total in shelf")]
+        public int TotalInShelf { get; set; }
+        [Display(Name = "Total in vault")]
+        public int TotalInVault { get; set; }
+        public int StoreId { get; set; }
+        [ForeignKey("StoreId")]
+        public virtual Store Store { get; set; }
+    }
+}
---
+++ ShoeStore/Models/Store.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ShoeStore.Models
+{
+    public class Store
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public virtual ICollection<Article> Articles { get; set; }
+
+        /*public Store()
+        {
+            this.Articles = new HashSet<Article>();
+        }*/
+    }
+}
